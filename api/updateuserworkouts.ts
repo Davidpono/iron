825814f0d1721867047671e1f32c0b4dbd73f45f
David Ponce <davidadanponce@outlook.com>
2024-11-26 14:47:14 -0500

    Update updateuserworkouts.ts

--- api/updateuserworkouts.ts
+++ api/updateuserworkouts.ts
@@ -1,5 +1,5 @@
 const axios = require('axios');
-
+//'https://discoveri.azurewebsites.net/api/userworkouts/'
 // Function to make a POST request to the userworkouts endpoint
 export async function addUserWorkout(userId:any, workoutId:any, workoutName:any) {
     const requestData = {
@@ -9,7 +9,7 @@ export async function addUserWorkout(userId:any, workoutId:any, workoutName:any)
     };
     console.log(requestData)
     try {
-        const response = await axios.post('https://discoveri.azurewebsites.net/api/userworkouts/', requestData);
+        const response = await axios.post('https://discoverii-main.onrender.com/api/userworkouts/', requestData);
 
         return response.data;
     } catch (error:any) {
